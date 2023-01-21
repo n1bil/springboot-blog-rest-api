3eb3ee0ecbc505f8036029ae922919a331d5b191
nabil <nabil.nadzor@gmail.com>
2023-01-21 23:30:08 +0100

    Validate create comment and update comment REST API request

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,5 +1,8 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,7 +11,20 @@ import lombok.Data;
 public class CommentDto {
 
     private long id;
+
+    // name must not be null or empty
+    @NotEmpty(message = "Name must not be null or empty")
     private String name;
+
+    // email must not be null or empty
+    // email fiend validation
+    @NotEmpty(message = "Email must not be null or empty")
+    @Email(message = "Must be a well-formed email address")
     private String email;
+
+    // comment message must not be null or empty
+    // comment message must be minimum 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Comment message must me minimum 10 characters")
     private String message;
 }
