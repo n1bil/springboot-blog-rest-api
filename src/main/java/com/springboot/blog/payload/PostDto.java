05ce4d4f01325d5087d40c69c1b45f5df64b55a7
nabil <nabil.nadzor@gmail.com>
2023-01-21 14:39:58 +0100

    Validate create post and update post rest api request

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -14,8 +16,21 @@ import java.util.Set;
 public class PostDto {
 
     private long id;
+
+    // title should not be null or empty
+    // title should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title must have at least 2 charters")
     private String title;
+
+    // post description should not be null or empty
+    // post description should have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description must have at least 10 characters")
     private String description;
+
+    // post content must not be null or empty
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
